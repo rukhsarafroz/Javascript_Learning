d4537b74ea75b4c03d74bf961ca26cfdb0ed49c6
rukhsarafroz <rukhsarafroz697@gmail.com>
2020-12-09 21:46:32 +0530

    iterator in javascript

---
+++ generatorsIterators.js
@@ -0,0 +1,24 @@
+function myIterators(start, finish){
+    let index = start;
+    let count = 0;
+    return{
+        next(){
+            if(index < finish){
+                const result = { value : index, done : false};
+                index += 1;
+                count++;
+                return result;
+            }
+            return{
+                value : count,
+                done : true
+            }
+        }
+    }
+}
+const it = myIterators(0,10);
+let result = it.next();
+while(!result.done){
+    console.log(result.value);
+    result = it.next();
+}
