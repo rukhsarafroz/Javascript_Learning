7b62bc321bd316c5bdb54ee82fe79671ac70a453
rukhsarafroz <rukhsarafroz697@gmail.com>
2021-05-19 16:38:47 +0530

    count population from array of object

---
+++ countPopulation.js
@@ -0,0 +1,17 @@
+let array=[
+    {"Gujrat": 2000},
+    {"Maharashtra":3000},
+    {"Andhrapradesh":4000}
+]
+
+function  totalPopulation(array){
+    let total=0;
+    array.map(obj => {
+        let arr = Object.keys(obj);//using keys
+        total += obj[arr];
+        let arr = Object.values(obj);//using values
+        total += arr[0];
+    })
+  return total;
+}
+console.log(totalPopulation(array));
